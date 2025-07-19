f243f8062213914bc178ed222b58943805c6d8d9
Jay.Hyeon <hyinky@gmail.com>
2025-07-20 04:02:02 +0900

    fix: sequelize add KST

--- server/models/airportWaitTime.js
+++ server/models/airportWaitTime.js
@@ -1,5 +1,11 @@
 import { DataTypes, Model } from 'sequelize';
 import sequelize from '../db/sequelize.js';
+import dayjs from 'dayjs';
+import utc from 'dayjs/plugin/utc.js';
+import timezone from 'dayjs/plugin/timezone.js';
+
+dayjs.extend(utc);
+dayjs.extend(timezone);
 
 class AirportWaitTime extends Model {}
 
@@ -12,11 +18,20 @@ AirportWaitTime.init({
   wait_b: DataTypes.INTEGER,
   wait_c: DataTypes.INTEGER,
   wait_d: DataTypes.INTEGER,
+  created_at_kst: {
+    type: DataTypes.DATEONLY,
+    allowNull: true
+  },
 }, {
   sequelize,
   modelName: 'airport_wait_time',
   tableName: 'airport_wait_time',
   timestamps: false,
+});
+
+AirportWaitTime.beforeCreate((instance) => {
+  const kstDate = dayjs(instance.created_at).tz('Asia/Seoul').format('YYYY-MM-DD');
+  instance.created_at_kst = kstDate;
 });
 
 export default AirportWaitTime;
