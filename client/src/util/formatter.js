ec4416c36ece50af270658c3a9650aec5197b41d
Jay.Hyeon <hyinky@gmail.com>
2025-07-22 13:34:35 +0900

    fix: delete client (react)

--- client/src/util/formatter.js
+++
@@ -1,6 +0,0 @@
-function formatSeconds(seconds) {
-    const hours = Math.floor(seconds / 3600);
-    const mins = Math.floor(seconds / 60) % 60;
-    return `${String(hours).padStart(2, '0')}:${String(mins).padStart(2, '0')}`;
-}
-export default formatSeconds;
